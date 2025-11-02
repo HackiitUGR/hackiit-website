48d3cc891ede067f86c900d863158c96cea03cb4
ivanrdgz03 <ivanfrisonivan@gmail.com>
2025-11-02 01:24:37 +0100

    Footer reorganizado y simplificado. El header también fue simplificado y se arregló un fallo de contraste en el logo cuando se salia del modo oscuro

--- .astro/astro/content.d.ts
+++ .astro/astro/content.d.ts
@@ -4,7 +4,6 @@ declare module 'astro:content' {
 			Content: import('astro').MarkdownInstance<{}>['Content'];
 			headings: import('astro').MarkdownHeading[];
 			remarkPluginFrontmatter: Record<string, any>;
-			components: import('astro').MDXInstance<{}>['components'];
 		}>;
 	}
 }
@@ -169,5 +168,5 @@ declare module 'astro:content' {
 
 	type AnyEntryMap = ContentEntryMap & DataEntryMap;
 
-	export type ContentConfig = typeof import("./../../src/content/config.js");
+	export type ContentConfig = typeof import("../../src/content/config.js");
 }
