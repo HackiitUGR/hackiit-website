6a47f4952b6130247b0c471aef086baeaa459792
ivanrdgz03 <ivanfrisonivan@gmail.com>
2025-11-05 00:03:08 +0100

    Fotos agregadas e instagram y correo comentados

--- src/navigation.ts
+++ src/navigation.ts
@@ -40,9 +40,9 @@ export const footerData = {
   socialLinks: [
     { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://x.com/hackiit_ugr' },
     { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: 'https://www.linkedin.com/company/hackiit' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
+    //{ ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/HackiitUGR' },
-    { ariaLabel: 'Email', icon: 'tabler:mail', href: 'mailto:hackiit@example.com' },
+    //{ ariaLabel: 'Email', icon: 'tabler:mail', href: 'mailto:hackiit@example.com' },
   ]
 };
